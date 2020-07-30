5e376a0c4c98153202ffefb794fbf495015ef1e4
Leo <medinayleo@gmail.com>
2020-07-29 20:50:26 -0400

    O(1)-space reverse string...I think?

--- interview_cake/js/reverseString.js
+++ interview_cake/js/reverseString.js
@@ -1,7 +1,7 @@
 /** String must be reversed in place */
 
 function revString(string) {
-  return string.slice(0, -1)
+  return string.split("").reverse().join("")
 }
 
 console.log(revString("Hello"))
