9aeac07ad64aaf08580e6308d6bb5b9977200284
Leo <medinayleo@gmail.com>
2020-07-29 21:03:05 -0400

    i'm an idiot, this should have been reversing an ARRAY not a string. The array reverse is now O(1)

--- interview_cake/js/reverseString.js
+++ interview_cake/js/reverseString.js
@@ -1,7 +1,37 @@
 /** String must be reversed in place */
 
+/**
+ * 
+ * Complexity 
+ * Space: O(n)
+ * Time: O(n)
+ * 
+ */
 function revString(string) {
-  return string.split("").reverse().join("")
+  return string.split("").reverse().join("");
 }
 
-console.log(revString("Hello"))
+/**
+ * 
+ * Complexity 
+ * Space: O(1)
+ * Time: O(n)
+ * 
+ */
+function reverse(array) {
+  let leftIndex = 0;
+  let rightIndex = array.length - 1;
+
+  while (leftIndex < rightIndex) {
+
+    const temp = array[leftIndex]
+    
+    array[leftIndex] = array[rightIndex];
+    array[rightIndex] = temp
+
+    leftIndex++;
+    rightIndex--;
+  }
+}
+
+reverse(['h','e','l','l','l'])
