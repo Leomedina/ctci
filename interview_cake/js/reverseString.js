c3c0acd1544623226e6ab18901cef955b4bbac5b
Leo <medinayleo@gmail.com>
2020-07-29 20:46:22 -0400

    reversed string in python, and (not-working) reverse string in Javascript (must be in-place/O(1) space)

---
+++ interview_cake/js/reverseString.js
@@ -0,0 +1,7 @@
+/** String must be reversed in place */
+
+function revString(string) {
+  return string.slice(0, -1)
+}
+
+console.log(revString("Hello"))
---
+++ interview_cake/py/reverseString.py
@@ -0,0 +1,6 @@
+# Reverse a string in place.
+
+def revString(string):
+  return string[::-1]
+
+print(revString("hello"))
