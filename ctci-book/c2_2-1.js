e6b0c1132190c74c65205706232d986e9c842c8c
Leo <medinayleo@gmail.com>
2020-08-12 15:56:55 -0400

    completed linked list problem 2.1

--- ctci-book/c2_2-1.js
+++ ctci-book/c2_2-1.js
@@ -11,45 +11,62 @@
 
 const LinkedList = require("../utilities/LinkedList");
 
-const linkedList = new LinkedList([1, 2, 3, 2, 4, 5, 1]); // values are converted to a linked list
+const testArr = [1, 2, 2, 3, 2, 5, 6, 3];
+const linkedList = new LinkedList(testArr); // values are converted to a linked list
+console.log(linkedList.toArray());
+console.log(removeDupsFromArray(testArr));
+
+/**
+ * 
+ * Complexity:
+ * Time = O(n) - Since we're only traversing the linked list once
+ * Space = O(n ^ 2) - I think? since we have a buffer
+ * 
+ */
 
 function removeDups(linkedList) {
-  let currentNode = linkedList.head
+  let currentNode = linkedList.head;
+  let prevNode = linkedList.head;
+  let buffer = {}
 
-  return currentNode;
+  while (currentNode) {
+    if (!buffer[currentNode.value]) {
+      buffer[currentNode.value] = currentNode.value;
+    } else {
+      prevNode.next = currentNode.next;
+      currentNode = prevNode;
+    }
+    prevNode = currentNode;
+    currentNode = currentNode.next;
+  }
+  return linkedList.toArray();
 }
 
 console.log(removeDups(linkedList));
 
+/**
+ * 
+ * Bad Solution - it's doubly bad since it's an implementation for a list and not a LinkedList
+ * -> good for double checking the removeDups functions
+ * 
+ * Complexity:
+ * time = O(n ^ 2) - looping through the entire array at least twice 
+ * space = O(n ^ 3) - not great since I'm adding 2 buffers.
+ *
+ */
 
-// /**
-//  * 
-//  * Bad Solution - it's doubly bad since it's an implementation for a list and not a LinkedList
-//  * 
-//  * Complexity:
-//  * time = O(n ^ 2) - looping through the entire array at least twice 
-//  * space = O(n ^ 3) - not great since I'm adding 2 buffers.
-//  *
-//  */
 
-// const testArr = [1, 2, 2, 3, 2, 5, 6, 1];
+function removeDupsFromArray(array) {
+  const buffer = {};
+  const retArr = [];
 
-// function removeDups(array) {
-//   const buffer = {};
-//   const retArr = [];
+  for (const num of array) {
+    if (!buffer[num]) buffer[num] = num;
+  }
 
-//   for (const num of array) {
-//     if (!buffer[num]) buffer[num] = num;
-//   }
+  for (const key in buffer) {
+    retArr.push(key);
+  }
 
-//   for (const key in buffer) {
-//     retArr.push(key);
-//   }
-
-//   return retArr;
-// }
-
-// function removeDupsBetter(array) {
-
-// }
-
+  return retArr;
+}
