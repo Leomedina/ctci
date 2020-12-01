b60d2cb9dafba18e1d6b956ea3429556d83b7fcb
Leo <medinayleo@gmail.com>
2020-11-30 19:29:17 -0500

    78 boilerplate

---
+++ lc/78.Subsets.js
@@ -0,0 +1,19 @@
+/**
+ * 
+ * 78. Subsets
+ * 
+ * Given an integer array nums, return all possible subsets (the power set).
+ *    The solution set must not contain duplicate subsets.
+ * 
+ */
+
+
+ /**
+  * findSubsets
+  * @param {number} nums
+  * @return {number[]} 
+  */
+
+ function findSubsets(nums){
+   return true;
+ };
