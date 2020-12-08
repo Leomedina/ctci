2b4cc2b54fdffb005d3137e5d386a03a44e9e551
Leo <medinayleo@gmail.com>
2020-12-07 19:07:39 -0500

    linear search

---
+++ patterns/search-algos/linear-search.js
@@ -0,0 +1,9 @@
+/** 
+ * Implement a basic linear search
+ */
+
+function linearSearch(array, target) {
+  for (let i = 0; i <= array.length; i++)
+    if (array[i] === target) return i;
+  return -1;
+};
