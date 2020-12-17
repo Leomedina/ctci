96588c9211e9d20e76b460ff67049e3e2eedbf05
Leo <medinayleo@gmail.com>
2020-12-17 14:22:04 -0500

    two-pointers

--- README.md
+++ README.md
@@ -1,8 +1,1 @@
-# <i>Actually</i> Crack the Coding Interview
-> A collection of tips and notes for the technical and non-technical aspects of the interview process
-
-## 1) Behavioral
-  - Work in Progress.
-
-## 2) Technical
-  - You can find some answers and notes to interview cake questions, Project Euler, and other sources. More to come soon.
+# A collection of tips and notes for the technical and non-technical aspects of the interview process
---
+++ ud/multiple-pointers/CountUniqueVals.js
@@ -0,0 +1,24 @@
+/**
+ *  Count Unique Values
+ *
+**/
+
+function countUniqueValues(array) {
+  if (array.length === 0) return 0;
+  let i = 0;
+  let j = 1;
+  let res = 0;
+  while (j <= array.length) {
+    if (array[j] === array[i]) j++;
+    else {
+      i = j;
+      res++;
+    };
+  };
+  return res;
+};
+
+console.log(countUniqueValues([1, 1, 1, 1, 2]))// 2
+console.log(countUniqueValues([1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13]))// 7
+console.log(countUniqueValues([])) // 0
+console.log(countUniqueValues([-2, -1, -1, 0, 1]))// 4
