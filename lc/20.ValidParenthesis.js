1fac5e87834b64c8023682ad480ea4c87312789f
Leo <medinayleo@gmail.com>
2021-01-26 14:18:24 -0500

    LC20 Valid Parenthesis

---
+++ lc/20.ValidParenthesis.js
@@ -0,0 +1,37 @@
+/**  LC 20 is Valid Parenthesis */
+
+function isValidParen(s) {
+  if (s.length < 2) return false;
+  const stack = [];
+
+  for (let i of s) {
+    let temp = stack.pop();
+
+    if (temp === '(' && i === ')') continue;
+    if (temp === '[' && i === ']') continue;
+    if (temp === '{' && i === '}') continue;
+    if (temp) stack.push(temp);
+    stack.push(i);
+  }
+
+  if (stack.length > 0) return false;
+  return true;
+};
+
+console.log(isValidParen("([]){"));
+
+
+// function isValidParen(s) {
+//   if (s.length < 2) return false;
+//   let i = 0;
+//   let j = 1;
+
+//   while (i < s.length) {
+//     if (s[i] === '(' && s[j] !== ')') return false;
+//     if (s[i] === '[' && s[j] !== ']') return false;
+//     if (s[i] === '{' && s[j] !== '}') return false;
+//     i = i + 2;
+//     j = j + 2;
+//   };
+//   return true;
+// };
